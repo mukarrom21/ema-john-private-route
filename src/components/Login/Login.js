814272eefc920edf026f9df6f26e2a15fc028609
mukarrom21 <mukarrom21@gmail.com>
2022-04-14 00:50:29 +0600

    Required added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 import "./Login.css";
 
@@ -12,6 +12,8 @@ const Login = () => {
     useSignInWithEmailAndPassword(auth);
 
   const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
 
   // handle the Email
   const handleEmailBlur = (event) => {
@@ -24,7 +26,7 @@ const Login = () => {
 
   // handle if user exist before
   if (user) {
-    navigate("/shop");
+    navigate(from, { replace: true });
   }
 
   const handleUserSignIn = (event) => {
